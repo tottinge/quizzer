7edc0ce09a118d0130c0c0290d7486de46acf1d5
Tim Ottinger <tottinge@industriallogic.com>
2020-07-16 10:12:42 -0500

    Converted to use a json-like document (as a refactoring, no less!)

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from bottle import route, run
 
 @route('/')
-def render_html(title='Magically Delicious'):
+def render_question(question):
+    title = question['title']
+    questions = question['questions']
+    asked = questions[0].get('question', 'nothing asked')
+    answers = questions[0].get('answers', [])
+    return render_html(title=title, question=asked)
+
+
+def render_html(title='Magically Delicious', question="are you happy?"):
     return f"""
 <!DOCTYPE html>
 <html>
@@ -9,7 +17,12 @@ def render_html(title='Magically Delicious'):
 <title>{title}</title>
 </head>
 <body>
-
+<section>
+<h1 class="page-title">{title}<h1>
+</section>
+<selection>
+<p class='question-asked'>{question}</p>
+</selection>
 </body>
 
 </html>"""
--- test_me.py
+++ test_me.py
@@ -1,7 +1,7 @@
 import unittest.mock
 from bs4 import BeautifulSoup
 
-from main import render_html
+from main import render_html, render_question
 
 
 class TestSomeStuff(unittest.TestCase):
@@ -9,9 +9,34 @@ class TestSomeStuff(unittest.TestCase):
     def test_document_should_be_html(self):
         pass
 
-    def test_title_appears_in_page_body(self):
+    def test_title_appears_as_title(self):
         title = "fred"
-        html_doc = render_html(title)
-        soup = BeautifulSoup(html_doc, 'html.parser')
+        soup = self.render(title=title)
         found = soup.head.find('title').text
         self.assertIn(title, found, f"Did not find {title} in [{found}]")
+
+    def test_title_appears_in_page_body(self):
+        title = "fred"
+        soup = BeautifulSoup(render_html(title), 'html.parser')
+        body_text = soup.body.text
+        self.assertIn(title, body_text)
+
+    def question_appears_in_selection_body(self):
+        question = 'Does this question appear?'
+        soup = self.render(question=question)
+        selection = soup.body.selection
+        self.assertIn(question, selection.text)
+
+    def render(self, title="_", question='?', answers=[]):
+        document = {
+            'title':title,
+            'questions':[
+                {
+                    'question':question,
+                    'answers':answers
+                }
+            ]
+        }
+        markup = render_question(document)
+        soup = BeautifulSoup(markup, 'html.parser')
+        return soup
