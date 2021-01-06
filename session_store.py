8f1594d56500f3bc38b6dbffc4995821178945a6
Tim Ottinger <tottinge@industriallogic.com>
2021-01-06 09:09:40 -0600

    simplified listcomps, cleared up some pylint warnings

--- session_store.py
+++ session_store.py
@@ -2,19 +2,21 @@ class SessionStore:
     """
     Store user sessions for later analysis and reporting
     """
+
     def __init__(self):
         self.recorded_answers = []
-        
+
     def record_answer(self, session_id, quiz_name, question_number, selection, is_correct):
-        self.recorded_answers.append( (session_id, quiz_name, question_number, selection, is_correct))
+        record = (session_id, quiz_name, question_number, selection, is_correct)
+        self.recorded_answers.append(record)
 
     def perfect_answers(self, session_id, quiz_name):
-        return [ item for item in self.recorded_answers
-                 if item[-1] == True and item[1] == quiz_name and item[0] == session_id]
+        return [item for item in self.recorded_answers
+                if item[0] == session_id and item[-1] is True]
 
     def incorrect_answers(self, session_id, quiz_name):
-        return [ item for item in self.recorded_answers
-                 if item[-1] == False and item[1] == quiz_name and item[0] == session_id]
+        return [item for item in self.recorded_answers
+                if item[0] == session_id and item[-1] is False]
 
     def number_of_correct_answers(self, session_id, quiz_name):
         return len(self.perfect_answers(session_id, quiz_name))
@@ -22,7 +24,7 @@ class SessionStore:
     def number_of_incorrect_answers(self, session_id, quiz_name):
         return len(self.incorrect_answers(session_id, quiz_name))
 
-    def get_new_session_id(self):
+    @staticmethod
+    def get_new_session_id():
         import uuid
         return str(uuid.uuid4())
-    
