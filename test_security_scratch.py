8543c3ef13cc8166872dc3c2adadba8c1abc652c
Tim Ottinger <tottinge@industriallogic.com>
2021-06-14 09:05:50 -0500

    cork first steps

--- test_security_scratch.py
+++ test_security_scratch.py
@@ -6,6 +6,7 @@ from cork import Cork
 class MyTestCase(unittest.TestCase):
     def test_create_a_cork(self):
         cork = Cork(directory='./security')
+        pass
 
 if __name__ == '__main__':
     unittest.main()
