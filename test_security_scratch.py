857218a5ece4e70d67a2fac14f5d1edfd74d1e9d
Perry Reid <perry.reid@retinalcare.com>
2021-06-15 09:04:05 -0500

    Continues to research the use of Cork for authentication

--- test_security_scratch.py
+++ test_security_scratch.py
@@ -6,6 +6,7 @@ from cork import Cork
 class MyTestCase(unittest.TestCase):
     def test_create_a_cork(self):
         cork = Cork(directory='./security')
+        result = cork.login(username='user',password='user')
         pass
 
 if __name__ == '__main__':
