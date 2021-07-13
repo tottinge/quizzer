4e3c48cd2085a515cc5e3c4cf577297415140f15
Tim Ottinger <tottinge@industriallogic.com>
2021-07-13 08:39:09 -0500

    Added form to quiz_authoring_form, refactored test to use dom

--- tests/test_quiz_authoring_form.py
+++ tests/test_quiz_authoring_form.py
@@ -5,18 +5,28 @@ from bs4 import BeautifulSoup
 from hamcrest import assert_that, contains_string, not_none
 
 
-class MyTestCase(unittest.TestCase):
+class StaticFormVerification(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.title = 'Edit Quiz'
+        cls.html = template('quiz_authoring_form.tpl', {'title': cls.title})
+        cls.dom = BeautifulSoup(cls.html, "html.parser")
+
     def test_title_is_displayed(self):
-        title = 'Edit Quiz'
-        html = template('quiz_authoring_form.tpl', {'title': title})
-        assert_that(html, contains_string(title))
+        dom = self.dom
+        tab_title = dom.head.title.text
+        assert_that(tab_title, contains_string(self.title))
+        visible_title = dom.body.header.find('h1', id='title')
+        assert_that(visible_title.text, contains_string(self.title))
 
     def test_form_exists(self):
-        title = 'Edit Quiz'
-        html = template('quiz_authoring_form.tpl', {'title': title})
-        dom = BeautifulSoup(html, "html.parser")
-        form = dom.body.find('form',id='quiz_edit')
+        form = self.dom.body.find('form',id='quiz_edit')
         assert_that(form, not_none())
+
+    def test_form_has_fields(self):
+        name_id = 'quiz_name'
+        title_id = 'quiz_title'
 
 
 if __name__ == '__main__':
--- views/quiz_authoring_form.tpl
+++ views/quiz_authoring_form.tpl
@@ -1,1 +1,5 @@
 % rebase('skeleton.tpl', title=title)
+
+<form id="quiz_edit">
+
+</form>
