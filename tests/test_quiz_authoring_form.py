b70747fe60af1cc52878c0fa1ccb39e72ba05190
Tim Ottinger <tottinge@industriallogic.com>
2021-07-13 09:04:53 -0500

    added some field checks, cleaned up after a bad refactoring

--- tests/test_quiz_authoring_form.py
+++ tests/test_quiz_authoring_form.py
@@ -4,29 +4,34 @@ from bottle import template
 from bs4 import BeautifulSoup
 from hamcrest import assert_that, contains_string, not_none
 
+from quizzes.quiz import Quiz
 
 class StaticFormVerification(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.title = 'Edit Quiz'
-        cls.html = template('quiz_authoring_form.tpl', {'title': cls.title})
+        cls.quiz = Quiz('','')
+        cls.page_title = 'Edit Quiz'
+        cls.html = template('quiz_authoring_form.tpl', {
+            'title': cls.page_title,
+        })
         cls.dom = BeautifulSoup(cls.html, "html.parser")
 
     def test_title_is_displayed(self):
         dom = self.dom
         tab_title = dom.head.title.text
-        assert_that(tab_title, contains_string(self.title))
+        assert_that(tab_title, contains_string(self.page_title))
         visible_title = dom.body.header.find('h1', id='title')
-        assert_that(visible_title.text, contains_string(self.title))
+        assert_that(visible_title.text, contains_string(self.page_title))
 
     def test_form_exists(self):
         form = self.dom.body.find('form',id='quiz_edit')
         assert_that(form, not_none())
 
     def test_form_has_fields(self):
-        name_id = 'quiz_name'
-        title_id = 'quiz_title'
+        form = self.dom.form
+        assert_that(form.find('input', id='quiz_name'), not_none())
+        assert_that(form.find('input', id='quiz_title'), not_none())
 
 
 if __name__ == '__main__':
--- views/quiz_authoring_form.tpl
+++ views/quiz_authoring_form.tpl
@@ -1,5 +1,9 @@
 % rebase('skeleton.tpl', title=title)
 
 <form id="quiz_edit">
+    <label for="quiz_name">Name</label>
+    <input type="text" id="quiz_name" value="">
 
+    <label for="quiz_title">Title</label>
+    <input type="text" id="quiz_title">
 </form>
