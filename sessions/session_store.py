26172bbc7f0d7ca98fd0d43540fe29bcb3c983d4
Tim Ottinger <tottinge@industriallogic.com>
2021-07-28 18:31:42 -0500

    Converted log type AnswerEntry to a dataclass

--- sessions/session_store.py
+++ sessions/session_store.py
@@ -1,5 +1,7 @@
 import logging
+from dataclasses import dataclass, field, asdict
 from datetime import datetime
+from typing import List
 
 import tinydb
 from tinydb import Query
@@ -7,49 +9,18 @@ from tinydb import Query
 logger = logging.getLogger(__name__)
 
 
+@dataclass
 class AnswerEntry:
-    """
-    Record an answer given to a question in a test session.
-    Whole-value class allows for evolution of records without breaking existing code.
-    """
-
-    def __init__(self, session_id, quiz_name, question_number, selection,
-                 is_correct, timestamp=None):
-        self.session_id = session_id
-        self.quiz_name = quiz_name
-        self.question_number = question_number
-        self.selection = selection
-        self.is_correct = is_correct
-        self.timestamp = timestamp or datetime.now().isoformat()
-
-    def __eq__(self, other):
-        return (
-                self.timestamp == other.timestamp
-                and self.session_id == other.session_id
-                and self.quiz_name == other.quiz_name
-                and self.question_number == other.question_number
-                and self.selection == other.selection
-                and self.is_correct == other.is_correct
-        )
-
-    def as_dict(self):
-        return dict(
-            timestamp=self.timestamp,
-            session_id=self.session_id,
-            quiz_name=self.quiz_name,
-            question_number=self.question_number,
-            selection=self.selection,
-            is_correct=self.is_correct
-        )
+    session_id: str
+    quiz_name: str
+    question_number: int
+    selection: str
+    is_correct: bool
+    timestamp: str = field(default_factory=lambda: datetime.now().isoformat)
 
     @classmethod
-    def from_dict(cls, record):
-        return AnswerEntry(session_id=record["session_id"],
-                           quiz_name=record["quiz_name"],
-                           question_number=record["question_number"],
-                           selection=record["selection"],
-                           is_correct=record["is_correct"],
-                           timestamp=record.get("timestamp", None))
+    def from_dict(cls, dictionary):
+        return AnswerEntry(**dictionary)
 
 
 class SessionStore:
@@ -68,9 +39,11 @@ class SessionStore:
 
     def record_answer(self, session_id, quiz_name, question_number, selection,
                       is_correct, timestamp=None):
+        if not timestamp:
+            timestamp = datetime.now().isoformat()
         record = AnswerEntry(session_id, quiz_name, question_number, selection,
                              is_correct, timestamp)
-        self.storage.insert(record.as_dict())
+        self.storage.insert(asdict(record))
 
     def perfect_answers(self, session_id, quiz_name):
         criteria = Query()
@@ -81,7 +54,7 @@ class SessionStore:
         )
         return [AnswerEntry.from_dict(x) for x in records]
 
-    def incorrect_answers(self, session_id, quiz_name):
+    def incorrect_answers(self, session_id, quiz_name) -> List[AnswerEntry]:
         criteria = Query()
         records = self.storage.search(
             (criteria.session_id == session_id)
