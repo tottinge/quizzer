536900aede43f26b5e160c2602c1150d69f3309f
Tim Ottinger <tottinge@industriallogic.com>
2021-09-13 08:16:04 -0500

    refactor: rename author controller test

--- apps/author/tests/test_authorcontroller.py
+++ apps/author/tests/test_authorcontroller.py
@@ -4,7 +4,7 @@ from apps.author.author_controller import AuthorController
 from shared.quizzology import Quizzology
 
 
-class MyTestCase(unittest.TestCase):
+class TestAuthorController(unittest.TestCase):
     def create_new_quiz(self):
         quizzology = Quizzology()
         x = AuthorController(quizzology)
