e1207e9fa2ce9efcb49aead1792e89beef0338a0
Tim Ottinger <tottinge@industriallogic.com>
2022-01-14 16:10:03 -0600

    started an authn test with webtest

--- security/tests/test_authn.py
+++ security/tests/test_authn.py
@@ -1,9 +1,15 @@
 import unittest
+from webtest import TestApp
+import main
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)  # add assertion here
+class TestLoginPage(unittest.TestCase):
+    def setUp(self):
+        self.app = TestApp(main.app)
+
+    def test_login_page_loads(self):
+        response = self.app.get("/login")
+
 
 
 if __name__ == '__main__':
