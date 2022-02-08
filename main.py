a5c3cd0346b0ff6f0049bb80bfbffe501bd10e53
Tim Ottinger <tottinge@industriallogic.com>
2022-02-08 12:11:59 -0600

    fix: request.forms() is a method, not an attribute. We must have been relying on some magic

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def authentication_endpoint():
                                httponly=True)
     bottle.response.set_cookie('qz-user-name', user.user_name)
     bottle.response.set_cookie('qz-user-role', user.role)
-    destination = request.forms.get('destination')
+    destination = request.forms().get('destination')
     redirect(destination)  # Todo - change landing page
 
 
