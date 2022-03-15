33f94be18b83f6e5734bdd003ffa78a1377e8c75
Tim Ottinger <tottinge@industriallogic.com>
2022-03-15 12:02:21 -0500

    feat: make it possible to see the form we've been editing

--- apps/author/author.py
+++ apps/author/author.py
@@ -10,22 +10,38 @@ Should support get, post, delete... which other verbs? Patch? Put?
 import bottle
 from bottle import view
 
+from apps.author.author_controller import AuthorController
 from quizzes.quiz import Quiz
+from shared.quizzology import Quizzology
 
 app = bottle.Bottle()
 app.resources.add_path("./apps/author/views")
 LOCAL_PATHS = ['./apps/author/views', *bottle.TEMPLATE_PATH]
 
+
 @app.route('/')
 @view('quiz_author_home', template_lookup=LOCAL_PATHS)
 def cover_page():
     return {
-        'title':'Welcome Author'
+        'title': 'Welcome Author'
     }
+
 
 @app.route('/quiz')
 def quiz_list():
     return "<h1>Quiz list</h1>"
+
+
+@app.get('/edit/<quiz_name>')
+@view('quiz_authoring_form', template_lookup=LOCAL_PATHS)
+def edit_existing(quiz_name: str):
+    # ToDo - this is no way to acquire a controller
+    author_controller = AuthorController(Quizzology())
+    return {
+        'title': 'Edit Existing Quiz',
+        'quiz': author_controller.get_quiz(quiz_name)
+    }
+
 
 @app.get('/edit')
 @view('quiz_authoring_form', template_lookup=LOCAL_PATHS)
--- apps/author/views/quiz_author_home.tpl
+++ apps/author/views/quiz_author_home.tpl
@@ -2,6 +2,7 @@
 <header>
     <nav class="w3-panel w3-bar w3-teal" id="menu">
          <a class="quiz-nav w3-bar-item w3-button" href="/study">Take a quiz</a>
+         <a class="quiz-nav w3-bar-item w3-button" href="/author/edit/catsquiz">Edit Cats</a>
          <a class="quiz-nav w3-bar-item w3-button" href="/author/edit">Edit a new quiz</a>
          <a class="quiz-nav w3-bar-item w3-button" href="/login">Log In Again</a>
     </nav>
