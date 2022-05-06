144fd33b7e01c345e9f3801ceef222e995d1740d
Perry Reid <wpreid@gmail.com>
2022-05-06 08:54:52 -0500

    fix: renamed some step_impl methods to eliminate errors found by mypy.

--- features/steps/AddNewQuiz.py
+++ features/steps/AddNewQuiz.py
@@ -8,7 +8,7 @@ from quizzes.quiz import Quiz
 
 
 @step("decoys are")
-def step_impl(context: Context):
+def step_impl_decoys_are(context: Context):
     only_row = context.table.row[0]
     context.decoys = [value.strip() for value in only_row.cells]
 
@@ -16,7 +16,7 @@ def step_impl(context: Context):
 
 
 @when('the author adds a quiz with name "{name}" and title "{title}"')
-def step_impl(context: Context, name: str, title: str):
+def step_impl_add_quiz(context: Context, name: str, title: str):
     quiz = Quiz(name=name, title=title)
     controller: AuthorController = context.author_controller
     result = controller.save(quiz)
@@ -24,39 +24,39 @@ def step_impl(context: Context, name: str, title: str):
 
 
 @then('"{quiz_name}" should be accessible')
-def step_impl(context: Context, quiz_name: str):
+def step_impl_quiz_accessible(context: Context, quiz_name: str):
     controller: AuthorController = context.author_controller
     assert_that(controller.quiz_exists(quiz_name), is_(True),
         f"Cannot access newly-created '{quiz_name}'")
 
 
 @when('the author adds a question "{question_text}"')
-def step_impl(context: Context, question_text):
+def step_impl_adds_question(context: Context, question_text):
     raise NotImplementedError(
         'STEP: When the author adds a question "Is there a question here?"')
 
 
 @step('the answer is "{answer}"')
-def step_impl(context: Context, answer):
+def step_impl_answer_is_correct(context: Context, answer):
     raise NotImplementedError('STEP: * the answer is "Not Yet"')
 
 
 @step('confirmation is "{confirmation}"')
-def step_impl(context: Context, confirmation):
+def step_impl_confirmation_is(context: Context, confirmation):
     raise NotImplementedError(
         'STEP: * confirmation is "There is one to come which will be added"')
 
 
 @then('there is one question in "{quiz_name}"')
-def step_impl(context: Context, quiz_name):
+def step_impl_there_is_one_question(context: Context, quiz_name):
     raise NotImplementedError('STEP: Then there is one question in "empty"')
 
 
 @when("the author adds resources")
-def step_impl(context: Context):
+def step_impl_adds_resource(context: Context):
     raise NotImplementedError('WHEN: the author adds resources')
 
 
 @then("the question has 2 resources")
-def step_impl(context: Context):
+def step_impl_question_has_resources(context: Context):
     raise NotImplementedError('STEP: Then the question has 2 resources')
