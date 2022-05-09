c25955ae3eb3d06b17b8b6a53dfbc20a957c7097
Tim Ottinger <tottinge@gmail.com>
2022-05-09 08:43:37 -0500

    reformat: as flake8 requested

--- apps/author/tests/test_quiz_schema.py
+++ apps/author/tests/test_quiz_schema.py
@@ -4,7 +4,7 @@ import unittest
 
 class SchemaValidator(unittest.TestCase):
     def test_schema_parses_without_raising_an_exception(self):
-        with open("static/quiz_schema.json") as schema_file :
+        with open("static/quiz_schema.json") as schema_file:
             json.load(schema_file)
 
 
