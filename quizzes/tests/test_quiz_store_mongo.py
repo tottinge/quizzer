3fbe9c4de01e29a411ad5f3db01e03d6cc21b4ac
Perry Reid <wpreid@gmail.com>
2022-06-02 07:34:38 -0500

    feat: Continued work on adding MongoDB QuizStore functionality and updated chromedriver

--- quizzes/tests/test_quiz_store_mongo.py
+++ quizzes/tests/test_quiz_store_mongo.py
@@ -1,20 +1,25 @@
+import os
 import unittest
-from unittest import skip
+from unittest import skip, skipIf
 from unittest.mock import MagicMock, patch
 
 from hamcrest import assert_that, is_
 
+from quizzes.quiz import Quiz
 from quizzes.quiz_store_mongo import QuizStoreMongo
 
 
 class QuizStoreMongoTest(unittest.TestCase):
-    @skip("Work In Progress")
+    @skipIf(os.environ.get("QUIZ_MONGO_URL", None) is None, "No database configured")
     def test_it_retrieves_a_quiz(self):
         test_db_name = "JustTesting"
         store = QuizStoreMongo(test_db_name)
+        store.save_quiz(Quiz("fred","What we said",[]))
         result = store.get_quiz('fred')
-        assert_that(result, is_(None))
-        store.db_connection().drop_database(test_db_name)
+        assert_that(result["name"], is_("fred"))
+        assert_that(result["title"], is_("What we said"))
+        # store.db_connection().drop_database(test_db_name)
+
 
 
 
