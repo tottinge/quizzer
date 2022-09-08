26fe627a1f7e649ce2680b9e5774c705632680da
Tim Ottinger <tottinge@gmail.com>
2022-09-08 14:02:05 +0100

    feat: build stub of first mongo session store method

--- apps/study/session_store_mongodb.py
+++ apps/study/session_store_mongodb.py
@@ -1,12 +1,24 @@
+from dataclasses import asdict
 from typing import List, Iterable
 
 from apps.study.session_store import SessionStore, AnswerEntry
+from shared.mongo_connection import db_connection
 
 
 class SessionStoreMongoDB(SessionStore):
     def record_answer(self, session_id, quiz_name, question_number, selection,
                       is_correct, question_id, timestamp=None):
-        pass
+        entry = AnswerEntry(
+            session_id, 
+            quiz_name, 
+            question_number, 
+            selection,
+            is_correct, 
+            question_id, 
+            timestamp)
+        with db_connection() as db:
+            collection = db['quizzology']['log']
+            collection.add(asdict(entry))
 
     def perfect_answers(self, session_id, quiz_name) -> List[AnswerEntry]:
         pass
