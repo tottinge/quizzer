27ee715a023c7d3aabdee8df381c17b0869aae68
Tim Ottinger <tottinge@industriallogic.com>
2023-06-12 14:03:52 +0200

    nope

--- session_scratch.py
+++ session_scratch.py
@@ -2,17 +2,17 @@ import bottle
 from beaker.middleware import SessionMiddleware
 
 session_opts = {
-    'session.type': 'memory',
-    'session.auto': True,
-    'session.cookie_expires': 300
+    "session.type": "memory",
+    "session.auto": True,
+    "session.cookie_expires": 300,
 }
 app = SessionMiddleware(bottle.app(), session_opts)
 
 
-@bottle.route('/test')
+@bottle.route("/test")
 def test():
-    s = bottle.request.environ.get('beaker.session')
-    s['test'] = s.get('test', 0) + 1
+    s = bottle.request.environ.get("beaker.session")
+    s["test"] = s.get("test", 0) + 1
     s.save()
     return f'Test counter {s.get("test")}'
 
