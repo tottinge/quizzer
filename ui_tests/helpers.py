2e09e7eaeed8fdcb2cf5b27aa041d372b7e4afac
Tim Ottinger <tottinge@industriallogic.com>
2024-03-25 14:17:58 +0000

    task: eliminate chromedriver downloads

--- ui_tests/helpers.py
+++ ui_tests/helpers.py
@@ -27,19 +27,18 @@ def launch_quizzology(port) -> Popen[str]:
     launch the quizzology application
     :param port:
     """
-    python = "./venv/bin/python" if os.path.isdir('./venv') else "python"
+    python = "./venv/bin/python" if os.path.isdir("./venv") else "python"
     return Popen(
         [python, "main.py"],
-        env={
-            **os.environ,
-            "QUIZ_PORT": str(port)
-        }
+        env={**os.environ, "QUIZ_PORT": str(port)},
+        shell=False,
+        text=True,
     )
 
 
-size_desktop = '1920,1200'
-size_mobile_iphone_X = '375,812'
-size_mobile_galaxy_s5 = '360,640'
+size_desktop = "1920,1200"
+size_mobile_iphone_X = "375,812"
+size_mobile_galaxy_s5 = "360,640"
 
 
 def launch_selenium_chrome(headless: bool) -> WebDriver:
@@ -47,26 +46,24 @@ def launch_selenium_chrome(headless: bool) -> WebDriver:
 
     options = Options()
     if headless:
-        options.add_argument('--headless')
-    options.add_argument('--window-size=%s' % size_mobile_galaxy_s5)
+        options.add_argument("--headless")
+    options.add_argument("--window-size=%s" % size_mobile_galaxy_s5)
     return webdriver.Chrome(options=options)
 
 
 def setup_path_for_dev_test():
-    project_root = os.environ.get('IDE_PROJECT_ROOTS', '.')
-    chrome_driver_path = os.path.join(project_root, 'webdrivers', sys.platform)
-    if os.path.isdir(chrome_driver_path):
-        path_env_value = os.environ['PATH']
-        path_seperator = os.pathsep
-        path_list = path_env_value.split(path_seperator)
-        if chrome_driver_path not in path_list:
-            path_list.append(chrome_driver_path)
-            os.environ['PATH'] = path_seperator.join(path_list)
+    import chromedriver_autoinstaller
+
+    project_root = os.environ.get("IDE_PROJECT_ROOTS", ".")
+    chrome_driver_path = os.path.join(project_root, "webdrivers", sys.platform)
+    os.makedirs(chrome_driver_path, exist_ok=True)
+    path = chromedriver_autoinstaller.install(path=chrome_driver_path)
+    os.environ["PATH"] = os.pathsep.join([os.environ["PATH"], path])
 
 
 def local_ip() -> str:
     hostname = gethostname()
-    return os.environ.get('QUIZ_HOST', gethostbyname(hostname))
+    return os.environ.get("QUIZ_HOST", gethostbyname(hostname))
 
 
 def get_likely_port() -> int:
@@ -76,7 +73,7 @@ def get_likely_port() -> int:
     Because it closes the socket, the system MAY HAVE ALREADY REUSED IT
     """
     junk_socket = socket()
-    host_name = os.environ.get('QUIZ_HOST', local_ip())
+    host_name = os.environ.get("QUIZ_HOST", local_ip())
     logging.debug(f"likely_port: Host name is {host_name}")
     junk_socket.bind((host_name, 0))
     _, chosen_port = junk_socket.getsockname()
@@ -90,6 +87,6 @@ def login(browser: WebDriver, url: str):
     WebDriverWait(browser, timeout=2, poll_frequency=0.25).until(
         condition.title_contains("Who are you")
     )
-    browser.find_element(By.NAME, 'user_name').send_keys('perry')
-    browser.find_element(By.NAME, 'password').send_keys('passme')
+    browser.find_element(By.NAME, "user_name").send_keys("perry")
+    browser.find_element(By.NAME, "password").send_keys("passme")
     browser.find_element(By.ID, "login").click()
